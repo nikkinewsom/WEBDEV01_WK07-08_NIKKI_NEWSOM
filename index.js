c9ac74b74ad09227cc534a0e464da968853595df
Nikki Newsom <nlnewsom@gmail.com>
2022-04-18 14:26:37 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer')
+const uuidv4 = require('uuid').v4
+const colors = require('colors')
+const Table = require('cli-table3')
+const _ = require('lodash')
+
+console.log / (uuidv4())
